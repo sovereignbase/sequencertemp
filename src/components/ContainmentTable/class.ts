1e820bb24c1a0709ea00bb1d16810d5191015e67
Jori Lethinen <lehtinenjori03@gmail.com>
2026-09-16 02:16:32 +0300

    chore: update containment table

--- src/components/ContainmentTable/class.ts
+++ src/components/ContainmentTable/class.ts
@@ -1,25 +1,28 @@
-import type { Strip, Delta } from '../types/type.js'
+import type { Insertion, Strip } from '../../types/type.js'
+
 export class ContainmentTable<T> {
   private readonly strips: Map<number, Map<number, Strip<T>>> = new Map()
 
-  has(incomingDelta: Delta<T>): boolean {
-    const actor = this.strips.get(incomingDelta[6])
-    if (actor) return actor.has(incomingDelta[7])
+  has(incomingInsertion: Insertion<T>): boolean {
+    const sequencer = this.strips.get(incomingInsertion[3])
+    if (sequencer) return sequencer.has(incomingInsertion[4])
     return false
   }
 
-  get(incomingDelta: Delta<T>): Strip<T> {
-    const actor = this.strips.get(incomingDelta[4])
-    if (!actor) return undefined
-    const containingStrip = actor.get(incomingDelta[5])
-    return containingStrip
+  get(incomingInsertion: Insertion<T>): Strip<T> {
+    const sequencer = this.strips.get(incomingInsertion[0])
+    if (!sequencer) return undefined
+
+    return sequencer.get(incomingInsertion[1])
   }
 
   set(incomingStrip: NonNullable<Strip<T>>): void {
-    const actor: Map<number, Strip<T>> = this.strips.get(
-      incomingStrip.actorY
-    ) ?? new Map()
-    if (actor.size === 0) void this.strips.set(incomingStrip.actorY, actor)
-    void actor.set(incomingStrip.timeY, incomingStrip)
+    const sequencer =
+      this.strips.get(incomingStrip.insertionSequencer) ?? new Map()
+
+    if (sequencer.size === 0)
+      void this.strips.set(incomingStrip.insertionSequencer, sequencer)
+
+    void sequencer.set(incomingStrip.insertionTime, incomingStrip)
   }
 }
