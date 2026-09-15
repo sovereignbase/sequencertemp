8a0297e2cd3ee22b3db188138f3a93dc8720a534
Jori Lethinen <lehtinenjori03@gmail.com>
2026-09-16 02:38:19 +0300

    chore: add create

--- src/algorithms/create.ts
+++ src/algorithms/create.ts
@@ -1,20 +1,19 @@
-import { Sequence } from '../class.js'
-import { isDelta } from '../helpers/index.js'
-import { Delta, Snapshot, Strip } from '../types/type.js'
+import { findVisibleIndexOfStrip } from '../auxiliary/findVisibleIndexOfStrip.js'
+import { insertAfter } from '../auxiliary/insertAfter.js'
+import { insertBefore } from '../auxiliary/insertBefore.js'
+import { insertFirst } from '../auxiliary/insertFirst.js'
+import { patchJumps } from '../auxiliary/patchJumps.js'
+import type { Sequence } from '../class.js'
+import type { Snapshot, Strip } from '../types/type.js'
 
 export function create<T>(
   this: Sequence<T>,
   actorID: number,
   trustedSnapshot?: unknown
 ): void {
-  let time: number = 0
+  let time = 0
 
-  const snapshot = Array.isArray(trustedSnapshot)
-    ? (trustedSnapshot as Snapshot<T>)
-    : undefined
-
-  const frontiers = snapshot?.[0]
-  const projection = snapshot?.[1]
+  const [frontiers, projection] = (trustedSnapshot as Snapshot<T>) ?? []
 
   if (Array.isArray(frontiers)) {
     // Actors must exist before acknowledgements are observed.
@@ -29,70 +28,99 @@ export function create<T>(
 
   const compactableIDs = new Set(this.frontierTable.getCompactableSessions())
 
-  let started: boolean = false
-
   if (Array.isArray(projection))
     for (const incoming of projection) {
-      if (!isDelta(incoming)) continue
+      // compact snapshot by not materializing compactable decreasing insertions
+      if (incoming[5] < 0 && compactableIDs.has(incoming[3])) continue
 
-      const delta = incoming as Delta<T>
-
-      // compact snapshot by not minting compactable masks
-      if (compactableIDs.has(delta[6])) continue
-
-      const [
-        type,
-        depencyPrefix,
-        initialLength,
-        offsetLength,
-        actorX,
-        timeX,
-        actorY,
-        timeY,
-        footage,
-      ] = delta
-
-      if (actorY === actorID) time = Math.max(time, timeY + initialLength + 1)
-
-      if (!started) {
-        const strip: NonNullable<Strip<T>> = {
-          type,
-          depencyPrefix,
-          initialLength,
-          offsetLength,
-          actorX,
-          timeX,
-          actorY,
-          timeY,
-          footage,
-        }
-
-        this.head = strip
-        this.gate = strip
-        this.tail = strip
-
-        this.structuralStripCount = 1
-        this.visibleFrameCount = initialLength
-        this.visibleIndex = 0
-
-        void this.containmentTable.set(strip)
-
-        started = true
-        continue
+      const incomingStrip: NonNullable<Strip<T>> = {
+        anchorSequencer: incoming[0],
+        anchorTime: incoming[1],
+        anchorFrame: incoming[2],
+        insertionSequencer: incoming[3],
+        insertionTime: incoming[4],
+        insertionDiff: incoming[5],
+        footage: incoming[6],
       }
 
-      this.patch(delta)
+      if (
+        incomingStrip.insertionDiff > 0 &&
+        incomingStrip.insertionSequencer === actorID
+      )
+        time = Math.max(time, incomingStrip.insertionTime)
+
+      const birth =
+        incomingStrip.anchorSequencer === 0 &&
+        incomingStrip.anchorTime === 0 &&
+        incomingStrip.anchorFrame === 0
+
+      if (birth && this.structuralStripCount === 0) {
+        insertFirst.call(this, incomingStrip)
+      } else {
+        let containingStrip: NonNullable<Strip<T>>
+        let targetFramePosition: number
+
+        if (birth) {
+          containingStrip = this.head!
+          targetFramePosition = 0
+        } else {
+          const origin = this.containmentTable.get(incoming)
+          if (!origin) continue
+
+          containingStrip = origin
+          targetFramePosition = incomingStrip.anchorFrame
+
+          while (true) {
+            const containingStripLength = Math.abs(
+              containingStrip.fragmentDiff ?? containingStrip.insertionDiff
+            )
+
+            if (targetFramePosition < containingStripLength) break
+
+            const rightFragment = containingStrip.rightFragment
+            if (!rightFragment) break
+
+            targetFramePosition -= containingStripLength
+            containingStrip = rightFragment
+          }
+        }
+
+        findVisibleIndexOfStrip.call(this, containingStrip)
+
+        const previousStructuralStripCount = this.structuralStripCount
+
+        if (targetFramePosition === 1)
+          insertBefore.call(this, incomingStrip, containingStrip)
+        else
+          insertAfter.call(
+            this,
+            incomingStrip,
+            containingStrip,
+            targetFramePosition
+          )
+
+        patchJumps.call(
+          this,
+          incomingStrip.insertionDiff,
+          this.structuralStripCount - previousStructuralStripCount
+        )
+      }
+
+      this.containmentTable.set(incomingStrip)
+
+      if (incomingStrip.insertionDiff > 0)
+        void this.frontierTable.observeActor(incomingStrip.insertionSequencer)
     }
 
   // The current actor becomes part of the frontier only after determining
   // what was already compactable in the stored snapshot.
   void this.frontierTable.observeActor(actorID)
 
-  this.insertClock[0] = actorID
-  this.insertClock[1] = time
+  this.increaseClock[0] = actorID
+  this.increaseClock[1] = time
 
-  this.maskClock[0] = this.frontierTable.getSafeSessionID()
-  this.maskClock[1] = 0
+  this.decreaseClock[0] = this.frontierTable.getSafeSessionID()
+  this.decreaseClock[1] = 0
 
   void this.frontierTable.freeCompactedSessions(Array.from(compactableIDs))
 }
