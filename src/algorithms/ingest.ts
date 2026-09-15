ad4151f55535236f4f00a6cd072f668628f80f6b
Jori Lethinen <lehtinenjori03@gmail.com>
2026-09-16 02:15:18 +0300

    chore: work on ingest

--- src/algorithms/ingest.ts
+++ src/algorithms/ingest.ts
@@ -1,7 +1,16 @@
 import type { Sequence } from '../class.js'
 import type { Delta } from '../types/type.js'
-import { isDelta } from '../auxiliary/isDelta.js'
+import {
+  isAcknowledgement,
+  isDelta,
+  isInsertion,
+} from '../auxiliary/isDelta.js'
 
 export function ingest<T>(this: Sequence<T>, data: unknown): Delta<T> | void {
-  if (!isDelta(data)) return
+  if (isInsertion(data)) {
+  }
+  if (isAcknowledgement(data)) {
+    return void this.frontierTable.observeAcknowledgement(data)
+  }
+  return
 }
