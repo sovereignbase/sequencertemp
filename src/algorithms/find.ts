5bfa2dae72e6362b50a036d4c4c376f8812cd6dd
Jori Lethinen <lehtinenjori03@gmail.com>
2026-09-16 05:01:01 +0300

    chore: fix finds

--- src/algorithms/find.ts
+++ src/algorithms/find.ts
@@ -2,5 +2,5 @@ import { findFrameByVisibleIndex } from '../auxiliary/findFrameByVisibleIndex.js
 import { Sequence } from '../class.js'
 
 export function find<T>(this: Sequence<T>, index: number): T | undefined {
-  return this.gate?.footage?.[findFrameByVisibleIndex.call(this, index)]
+  return this.gate?.footage?.[findFrameByVisibleIndex.call(this, index) - 1]
 }
