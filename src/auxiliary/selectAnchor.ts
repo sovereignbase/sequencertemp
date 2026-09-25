45e66cf1a6512f8d005192580afaf789b38c66ff
Jori Lethinen <lehtinenjori03@gmail.com>
2026-09-25 10:26:32 +0300

    chore: extend fix

--- src/auxiliary/selectAnchor.ts
+++ src/auxiliary/selectAnchor.ts
@@ -48,7 +48,7 @@ export function selectAnchor<T>(
 
     const leftStep = anchoringStrip.leftStep
     const fragmentStart = anchoringStrip.fragmentStart ?? 0
-    // If anchor 0 has already been consumed by a past head insertion, anchor after
+    // If anchor 0 has already been consumed by a past boundary insertion, anchor after
     // the insertion that consumed it by using that insertion's final Frame instead.
     if (
       leftStep &&
